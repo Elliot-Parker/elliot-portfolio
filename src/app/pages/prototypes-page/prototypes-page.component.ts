00fdb5a18a612abe115f1dae07c76322e8d1a577
Elliot Parker <pedro.-.contato@outlook.com>
2022-02-18 23:29:13 -0300

    new prototype sources

--- src/app/pages/prototypes-page/prototypes-page.component.ts
+++ src/app/pages/prototypes-page/prototypes-page.component.ts
@@ -13,6 +13,30 @@ export class PrototypesPageComponent implements OnInit {
             imageUrl: "../../../assets/images/hello.png",
             url: "https://player.vimeo.com/video/494310155?h=ab977db784&amp;badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479",
         },
+
+        {
+            name: "Business Connect",
+            imageUrl: "../../../assets/images/businessConnect.png",
+            url: "https://player.vimeo.com/video/494310155?h=ab977db784&amp;badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479",
+            description: `
+            Eu que fiz o design mas o Jean não usou`,
+        },
+
+        {
+            name: "Business Connect 02",
+            imageUrl: "../../../assets/images/businessConnect.png",
+            url: "https://player.vimeo.com/video/494310155?h=ab977db784&amp;badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479",
+            description: `
+            Eu que fiz o design mas o Jean não usou`,
+        },
+
+        {
+            name: "Business Connect 03",
+            imageUrl: "../../../assets/images/businessConnect.png",
+            url: "https://player.vimeo.com/video/494310155?h=ab977db784&amp;badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479",
+            description: `
+            Eu que fiz o design mas o Jean não usou`,
+        },
     ]
 
     constructor() { }
